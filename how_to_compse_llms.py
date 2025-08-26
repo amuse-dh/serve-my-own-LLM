ac98707c70a9cec0ec566cb0f29ec4dcdbb13ad1
amuse-dh <57606030+amuse-dh@users.noreply.github.com>
2025-08-26 15:48:35 +0900

    Update how_to_compse_llms.py

--- how_to_compse_llms.py
+++ how_to_compse_llms.py
@@ -1,14 +1,16 @@
 from langchain_openai import ChatOpenAI # 외부 자원 사용 - 사용량에 의한 결제
 from langchain_community.llms import Ollama # 내 자원 사용 - local 자원에 모델 올려서 사용
-from langchain.chains import LLMChain
-from langchain.prompts import PromptTemplate
-from langchain_core.prompts.few_shot import FewShotPromptTemplate
-from langchain.cache import InMemoryCache
-from langchain.globals import set_llm_cache
+from langchain.chains import LLMChain # chain 생성 
+from langchain.prompts import PromptTemplate # prompt template
+from langchain_core.prompts.few_shot import FewShotPromptTemplate # fewshot template persona, chat e.g, prompt
+from langchain.cache import InMemoryCache # 캐시 - 응답을 저장해서 같은 질문이 들어올 때 사용량 X
+from langchain.globals import set_llm_cache # 캐시 - 응답을 저장해서 같은 질문이 들어올 때 사용량 X
+from langchain.callbacks import get_openai_callback # 토큰 수 및 추징요금 추적
+
 
 llm = ChatOpenAI(
     temperature = 0,
-    openai_api_key="You can get your own api keys"
+    openai_api_key="your own api keys"
 )
 
 set_llm_cache(InMemoryCache())
@@ -22,25 +24,24 @@ example_prompt = PromptTemplate.from_template(
 )
 
 persona = """
-당신은 '추론의 달인'이라는 별명을 가진 AI입니다. 
-모든 질문에 대해 중간 질문을 나누어 단계적으로 사고하며, 
+당신은 한국인 ML,DL 교수 AI입니다.
+모든 질문에 대해 단계적으로 사고하며 정확한 연산과, 과정 그로 일어날 효과들에대해 
 논리적으로 판단하고, 마지막에 결론을 명확하게 말합니다.
-다음은 그런 방식으로 답한 예시들입니다.
 """
 
 prompt = FewShotPromptTemplate(
     examples=examples,
     example_prompt=example_prompt,
     prefix = persona,
-    suffix="Question:\n{question}\nAnswer:",
-    input_variables=["question"],
+    suffix="Question:\n{question}\nContext:\n{context}\nAnswer:",
+    input_variables=["question","context"],
 )
 
+db = Chroma(
+    persist_directory="./my_paper_vectordb",
+    embedding_function=embedding
+)
 # llm = Ollama(model="llama3")
 # chain = LLMChain(llm = llm, prompt = prompt)
 
 chain = prompt | llm | StrOutputParser()
-question = {"question":"당신도 인간처럼 생각할 수 있나요"}
-
-answer = chain.stream(question)
-stream_response(answer)
